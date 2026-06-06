d86e800ee8d9febadcfc7ae530a0bc2ceac31086
Dattu <chepurirajini0939k@gmail.com>
2026-06-06 14:41:15 +0530

    added movie metascore also

--- movieDetails/info.py
+++ movieDetails/info.py
@@ -15,5 +15,6 @@ while True:
     print("movie awards: ", data["Awards"])
     print("movie poster: ", data["Poster"])
     print("movie ratings: ", data["Ratings"])
+    print("movie metascore: ", data["Metascore"])
     if input("Do you want to search for another movie? (yes/no): ") == "no":
         break
