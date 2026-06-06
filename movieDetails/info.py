6f1785d6c657159134aae3318998483ec690d472
Dattu <chepurirajini0939k@gmail.com>
2026-06-06 14:44:25 +0530

    deleted and commiting the stuff

--- movieDetails/info.py
+++ movieDetails/info.py
@@ -18,4 +18,3 @@ while True:
     print("movie metascore: ", data["Metascore"])
     if input("Do you want to search for another movie? (yes/no): ") == "no":
         break
-print("lo for fubn ")    
